c5db15ed75eac718012f13661d09ecb968db11c1
Yeung Yiu Hung <hkclex@gmail.com>
2018-04-07 21:36:49 +0800

    Update skeleton

---
+++ example/dartodon_example.dart
@@ -0,0 +1,6 @@
+import 'package:dartodon/dartodon.dart';
+
+main() {
+  var awesome = new Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/dartodon.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dartodon;
+
+export 'src/dartodon_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
